817119eb0a9b90883c033651e7deae2a685acaa9
yusufelyldrm <yusufelyildirim@gmail.com>
2023-03-28 18:11:16 +0300

    handlers created

--- main.go
+++ main.go
@@ -5,14 +5,25 @@ import (
 	"net/http"
 )
 
+const portNumber = ":8080"
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+}
+
 func main() {
-	http.HandleFunc("/", reservationHandler)
 
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
+	fmt.Println(fmt.Sprintf("Starting application on port %s\n Press 'Ctrl + C' to stop", portNumber))
+	err := http.ListenAndServe(portNumber, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
-
-func reservationHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(fmt.Sprintf("Reservation request received: %s", r.URL.Path))
-}
-a
