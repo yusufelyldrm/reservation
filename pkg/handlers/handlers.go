2d564b575a59effe099723aa97d71d42ecfb3379
yusufelyldrm <yusufelyildirim@gmail.com>
2023-04-02 13:33:58 +0300

    Working with layouts

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -7,10 +7,10 @@ import (
 
 // About is the about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.gohtml")
 }
 
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.gohtml")
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,7 +8,9 @@ import (
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	// parse the template files
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.gohtml")
+	// execute the template
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template: ", err)
