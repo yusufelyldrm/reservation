43d13941f8c9bc2082ac58218b3a25737f1c48ea
yusufelyldrm <yusufelyildirim@gmail.com>
2023-04-04 10:48:35 +0300

    using chi package instead of pat

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/yusufelyldrm/reservation/pkg/config"
 	"github.com/yusufelyldrm/reservation/pkg/handlers"
 	"net/http"
 )
 
 func routes(config config.AppConfig) http.Handler {
-	mux := pat.New()
+	/*mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
 	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	*/
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
