80fe5a643b460a3d75a19c9712654d5690b3647a
yusufelyldrm <yusufelyildirim@gmail.com>
2023-04-04 12:29:23 +0300

    developing own middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,7 @@ func routes(config config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
--- go.mod
+++ go.mod
@@ -3,3 +3,4 @@ module github.com/yusufelyldrm/reservation
 go 1.19
 
 require github.com/go-chi/chi/v5 v5.0.8
+require github.com/justinas/nosurf v1.1.1 // indirect
