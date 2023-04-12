df5a3eb39bb6fb7d234b83c2392be9fe3a6c53ba
yusufelyldrm <yusufelyildirim@gmail.com>
2023-04-12 23:57:50 +0300

    errors ,forms , models created

---
+++ pkg/forms/errors.go
@@ -0,0 +1,19 @@
+package forms
+
+// Error is a type that holds a map of errors
+type errors map[string][]string
+
+// Add adds an error message to the map of errors
+func (e errors) Add(field, message string) {
+	e[field] = append(e[field], message)
+}
+
+// Get gets the first error message for a given field
+func (e errors) Get(field string) string {
+	// If there are no errors, return an empty string
+	es := e[field]
+	if len(es) == 0 {
+		return ""
+	}
+	return es[0]
+}
---
+++ pkg/forms/forms.go
@@ -0,0 +1,39 @@
+package forms
+
+import (
+	"net/http"
+	"net/url"
+)
+
+// Form creates a custom from struct which embeds an url.Values object
+type Form struct {
+	url.Values
+	Errors errors
+}
+
+// Valid returns true if there are no errors, otherwise false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0 // If there are no errors, return true
+}
+
+// New initializes a form struct
+func New(data url.Values) *Form {
+	// Return a pointer to a form struct containing the data
+	return &Form{
+		data,
+		// Initialize the errors map
+		errors(map[string][]string{}),
+	}
+}
+
+// Has checks if form field is in post is not empty
+func (f *Form) Has(field string, r *http.Request) bool {
+	// Check if the form field is in the post data
+	x := f.Get(field)
+	if x == "" {
+		// If it is not, add an error message
+		f.Errors.Add(field, "This field cannot be empty")
+		return false
+	}
+	return true
+}
