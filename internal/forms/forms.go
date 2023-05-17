78f8cf97dea2a38b4ab12b1de8e2b16dd34e3654
yusufelyldrm <yusufelyildirim@gmail.com>
2023-05-17 18:10:12 +0300

    using govalidator package for email validation

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -2,6 +2,7 @@ package forms
 
 import (
 	"fmt"
+	"github.com/asaskevich/govalidator"
 	"net/http"
 	"net/url"
 	"strings"
@@ -55,4 +56,11 @@ func (f *Form) Has(field string, r *http.Request) bool {
 		return false
 	}
 	return true
+}
+
+// IsEmail checks if email is valid
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
 }
