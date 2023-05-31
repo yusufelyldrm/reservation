df2994033a4597ed56d20d3469e6d3b9bb39f795
yusufelyldrm <yusufelyildirim@gmail.com>
2023-05-31 23:39:52 +0300

    inserting room restrictions

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -81,15 +81,19 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	startDate, err := time.Parse(layout, sd)
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
+
 	endDate, err := time.Parse(layout, ed)
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	reservation := models.Reservation{
@@ -116,12 +120,26 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 			Form: form,
 			Data: data,
 		})
+	}
+
+	newReservationID, err := m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
 		return
 	}
 
-	err = m.DB.InsertReservation(&reservation)
+	restriction := models.RoomRestriction{
+		StartDate:     startDate,
+		EndDate:       endDate,
+		RoomID:        roomID,
+		ReservationID: newReservationID,
+		RestrictionID: 1,
+	}
+
+	err = m.DB.InsertRoomRestriction(restriction)
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -11,14 +11,16 @@ func (m *postgresDBRepo) AllUsers() bool {
 }
 
 // InsertReservation inserts a reservation into the database
-func (m *postgresDBRepo) InsertReservation(res *models.Reservation) error {
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `insert into reservations(first_name,last_name,email,phone,star_date,end_date,room_id,created_at,updated_at)
-values ($1,$2,$3,$4,$5,$6,$7,$8,$9)` //
+	var newID int
 
-	_, err := m.DB.ExecContext(
+	stmt := `insert into reservations (first_name,last_name,email,phone,start_date,end_date,room_id,created_at,updated_at)
+values ($1,$2,$3,$4,$5,$6,$7,$8,$9) returning id `
+
+	err := m.DB.QueryRowContext(
 		ctx,
 		stmt,
 		res.FirstName,
@@ -28,8 +30,34 @@ values ($1,$2,$3,$4,$5,$6,$7,$8,$9)` //
 		res.StartDate,
 		res.EndDate,
 		res.RoomID,
-		res.CreatedAt,
-		res.UpdatedAt,
+		//res.CreatedAt,
+		//res.UpdatedAt,
+		time.Now(),
+		time.Now(),
+	).Scan(&newID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return newID, nil
+}
+
+// InsertRoomRestriction inserts a room restriction into the database
+func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := ` insert into room_restrictions (start_date,end_date,room_id,reservation_id,restriction_id,created_at,updated_at)
+ 	values ($1,$2,$3,$4,$5,$6,$7)`
+	_, err := m.DB.ExecContext(
+		ctx,
+		stmt,
+		r.StartDate,
+		r.EndDate,
+		r.RoomID,
+		r.ReservationID,
+		r.RestrictionID,
 		time.Now(),
 		time.Now(),
 	)
@@ -37,6 +65,5 @@ values ($1,$2,$3,$4,$5,$6,$7,$8,$9)` //
 	if err != nil {
 		return err
 	}
-
 	return nil
 }
