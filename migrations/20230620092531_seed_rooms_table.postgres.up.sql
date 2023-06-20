609ad1bad539a6b482cbebe7abf595c649b19e4a
yusufelyldrm <yusufelyildirim@gmail.com>
2023-06-20 12:44:06 +0300

    adding migrations for seeding rooms and restrictons table

---
+++ migrations/20230620092531_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at) VALUES
+('General''s Quarters', '2023-05-31 23:13:07.000000', '2023-05-31 23:13:09.000000'),
+('Major''s Suite', '2023-06-01 16:01:52.000000', '2023-06-01 16:01:53.000000');
---
+++ migrations/20230620093910_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+('Reservation','2023-05-31 23:10:11.000','2023-05-31 23:10:13.000'),
+('Owner Block','2023-06-01 15:42:00.000','2023-06-01 15:42:03.000');
