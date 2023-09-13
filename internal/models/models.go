a188125e8a73a7d8d58f342696a176e0ba93e4d8
yusufelyldrm <yusufelyildirim@gmail.com>
2023-09-13 21:58:12 +0300

    make email better format and send email to property owner

--- internal/models/models.go
+++ internal/models/models.go
@@ -64,8 +64,9 @@ type RoomRestriction struct {
 
 // MailData is the mail data model
 type MailData struct {
-	To      string
-	From    string
-	Subject string
-	Content string
+	To       string
+	From     string
+	Subject  string
+	Content  string
+	Template string
 }
